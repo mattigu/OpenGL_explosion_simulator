50edbde0e66323be3b5226c86ac18174653acfd6
mgulinsk <01187230@pw.edu.pl>
2025-05-28 19:59:57 +0200

    Configure assimp and add setup tutorial

--- GKOM_OpenGL_2/Model.h
+++ GKOM_OpenGL_2/Model.h
@@ -4,14 +4,27 @@
 #include <vector>
 
 #include "Mesh.h"
+#include "Program.h"
+
+#include <assimp/Importer.hpp>      // C++ importer interface
+#include <assimp/scene.h>           // Output data structure
+#include <assimp/postprocess.h>     // Post processing flags
 
 class Model
 {
-private:
-
 protected:
+	std::vector<Texture> textures_loaded;
+	std::vector<std::shared_ptr<Mesh>> meshes;
+	std::string directory;
 	
+	void loadModel(std::string const& path);
+	// assimp params below so keeping them empty 
+	void processNode(); 
+	Mesh processMesh();
+	std::vector<Texture> loadMaterialTextures();
+	unsigned int TextureFromFile();
 public:
+	virtual void Draw(Program& program);
 
 };
 
