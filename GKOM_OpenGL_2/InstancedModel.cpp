1a9b61b6b255aef5277ab4baa3d255d725e5e58c
mgulinsk <01187230@pw.edu.pl>
2025-05-30 13:20:16 +0200

    Added textures to instanced models

--- GKOM_OpenGL_2/InstancedModel.cpp
+++ GKOM_OpenGL_2/InstancedModel.cpp
@@ -1,1 +1,13 @@
 #include "InstancedModel.h"
+
+void InstancedModel::createMesh(const std::vector<Vertex>& vertices, const std::vector<unsigned int>& indices, const Texture& diffuseTexture)
+{
+	_meshes.push_back(std::make_shared<InstancedMesh>(vertices, indices, diffuseTexture, _modelMatrices));
+}
+
+void InstancedModel::Draw(Program& program) const
+{
+	for (const auto& meshPtr : _meshes) {
+		meshPtr->Draw(program);
+	}
+}
